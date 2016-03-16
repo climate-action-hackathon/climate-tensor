8d0f2b3bf6f7ceb33e5e0c0b5e1e03820b8d5d3b
lacunadream <d.s.yong@lse.ac.uk>
2016-03-16 18:43:24 +0200

    second level

--- thirtydayscrapper/base_urls.py
+++ thirtydayscrapper/base_urls.py
@@ -1,16 +1,30 @@
 import requests
 import bs4
+import threading
+import json
 
-root = "http://www.accuweather.com/en/browse-locations/afr/zm"
+# root = "http://www.accuweather.com/en/browse-locations/afr/zm"
 
-r = requests.get(root)
-# print(r.text)
+# r = requests.get(root)
+# # print(r.text)
+# source = bs4.BeautifulSoup(r.content, "lxml")
+
+# k = source.find("ul", {"class":"articles"})
+# kk = []
+# for link in k.findAll('a'):
+#     # print(link.get('href'))
+#     kk.append(link.get('href'))
+
+# print(kk)
+
+test = "http://www.accuweather.com/en/browse-locations/afr/zm/01"
+
+r = requests.get(test)
 source = bs4.BeautifulSoup(r.content, "lxml")
+p = source.find("ul", {"class":"articles"})
+pp = []
+for link in p.findAll('a'):
+	pp.append(link.get('href'))
 
-k = source.find("ul", {"class":"articles"})
-kk = []
-for link in k.findAll('a'):
-    # print(link.get('href'))
-    kk.append(link.get('href'))
+print(pp)
 
-print(kk)
