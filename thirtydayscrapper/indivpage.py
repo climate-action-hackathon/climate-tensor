e6d98eb422e5e12d8d9db0d3fa006d211f124784
lacunadream <d.s.yong@lse.ac.uk>
2016-03-16 23:33:52 +0200

    new attempt on monthly data

--- thirtydayscrapper/indivpage.py
+++ thirtydayscrapper/indivpage.py
@@ -5,54 +5,71 @@ import json
 
 test = "http://www.accuweather.com/en/zm/livingstone/355959/march-weather/355959?monyr=3/1/2016&view=table"
 
-r = requests.get(test)
-# print(r.text)
-source = bs4.BeautifulSoup(r.content, "lxml")
+with open('dict.txt', 'r') as loadfile:
+    pagesData = json.load(loadfile)
 
-indivjson = {}
-def dropList(x):
-	return ''.join(x)
-
-k = source.findAll("th", {"style": "background-color:#f6f7f8"})
-# print(k)
-count = 0
-for x in k:
-	count += 1
-	z = x.contents
-	str(z)
-	z = z[len(z)-13:len(z)-4]
-	z = dropList(z)
-
-	gg = x.findNextSibling('td')
-	ggg = gg.contents
-	ggg = dropList(ggg)
-	hi = ggg[:len(ggg)-1]
-
-	pp = gg.findNextSibling('td')
-	ppp = pp.contents
-	ppp = dropList(ppp)
-	lo = ppp[:len(ppp)-1]
-
-	ww = pp.findNextSibling('td')
-	www = ww.contents
-	www = dropList(www)
-	pre = www[:len(www)-1]
+for x in pagesData:
+	k = str(pagesData[x])
+	kk = str(pagesData[x]['id'])
+	construct = "http://www.accuweather.com/en/zm/" + x + "/" + kk + "/march-weather/" + kk +"?monyr=3/1/2016&view=table"
+	# construct = "http://www.accuweather.com/en/zm" + pagesData[x] + "/" + pagesData[x]['id'] + "/march-weather/" + pagesData[x]['id'] +"?monyr=3/1/2016&view=table"
+	print(construct)
 
 
-	# print(z)
-	print('lol', hi)
-	print(lo)
-	print(pre)
 
-	d = {
-		count: {
-			"lotemp": lo,
-			"hitemp": hi,
-			"pre" : pre,
-		}
-	}
+def indiScrap(link):
+	r = requests.get(link)
+	source = bs4.BeautifulSoup(r.content, "lxml")
 
-	indivjson.update(d)
 
-with open('livingstone_march.txt', 'w') as outfile:
-    json.dump(indivjson, outfile)
+# r = requests.get(test)
+# # print(r.text)
+# source = bs4.BeautifulSoup(r.content, "lxml")
+
+# indivjson = {}
+# def dropList(x):
+# 	return ''.join(x)
+
+# k = source.findAll("th", {"style": "background-color:#f6f7f8"})
+# # print(k)
+# count = 0
+# for x in k:
+# 	count += 1
+# 	z = x.contents
+# 	str(z)
+# 	z = z[len(z)-13:len(z)-4]
+# 	z = dropList(z)
+
+# 	gg = x.findNextSibling('td')
+# 	ggg = gg.contents
+# 	ggg = dropList(ggg)
+# 	hi = ggg[:len(ggg)-1]
+
+# 	pp = gg.findNextSibling('td')
+# 	ppp = pp.contents
+# 	ppp = dropList(ppp)
+# 	lo = ppp[:len(ppp)-1]
+
+# 	ww = pp.findNextSibling('td')
+# 	www = ww.contents
+# 	www = dropList(www)
+# 	pre = www[:len(www)-1]
+
+
+# 	# print(z)
+# 	print('lol', hi)
+# 	print(lo)
+# 	print(pre)
+
+# 	d = {
+# 		count: {
+# 			"lotemp": lo,
+# 			"hitemp": hi,
+# 			"pre" : pre,
+# 		}
+# 	}
+
+# 	indivjson.update(d)
+
+# with open('livingstone_march.txt', 'w') as outfile:
+#     json.dump(indivjson, outfile)
